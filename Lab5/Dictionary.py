f1f4cca42d641317aefa8ebef6cc1320ce43f3f1
Beverly B Mambou <bmambou@hawaii.edu>
2026-02-05 13:16:22 -1000

    Dictionaries

---
+++ Lab5/Dictionary.py
@@ -0,0 +1,19 @@
+country_capitals = {
+    "Indonesia": "Jakarta",
+    "Malaysia": "Kuala Lumpur",
+    "Philippines": "Manila"}
+
+print(country_capitals)
+
+print(country_capitals["Indonesia"])
+print(country_capitals["Malaysia"])
+
+country_capitals ["Mongolia"] = "Ulaanbaatar"
+print(country_capitals)
+
+country_capitals["Indonesia"] = "Bali"
+print(country_capitals)
+
+print("Indonesia" in country_capitals)
+print("Thailand" not in country_capitals)
+print("Korea" in country_capitals)
